0dd6fd3aaaac56e6672e3555fba3c243b0cce26c
Lucia-Gomez327 <luciagomez@yahoo.com>
2025-11-02 23:28:38 -0300

    Fix Agregar archivos faltantes y corregir

--- src/main/java/com/starwars/domain/model/SwapiPageResponse.java
+++ src/main/java/com/starwars/domain/model/SwapiPageResponse.java
@@ -1,7 +1,16 @@
 package com.starwars.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SwapiPageResponse<T> {
     private String message;
     private Integer totalRecords;
@@ -9,66 +18,5 @@ public class SwapiPageResponse<T> {
     private String previous;
     private String next;
     private List<T> results;
-    
-    public SwapiPageResponse() {
-    }
-    
-    public SwapiPageResponse(String message, Integer totalRecords, Integer totalPages, 
-                            String previous, String next, List<T> results) {
-        this.message = message;
-        this.totalRecords = totalRecords;
-        this.totalPages = totalPages;
-        this.previous = previous;
-        this.next = next;
-        this.results = results;
-    }
-    
-    public String getMessage() {
-        return message;
-    }
-    
-    public void setMessage(String message) {
-        this.message = message;
-    }
-    
-    public Integer getTotalRecords() {
-        return totalRecords;
-    }
-    
-    public void setTotalRecords(Integer totalRecords) {
-        this.totalRecords = totalRecords;
-    }
-    
-    public Integer getTotalPages() {
-        return totalPages;
-    }
-    
-    public void setTotalPages(Integer totalPages) {
-        this.totalPages = totalPages;
-    }
-    
-    public String getPrevious() {
-        return previous;
-    }
-    
-    public void setPrevious(String previous) {
-        this.previous = previous;
-    }
-    
-    public String getNext() {
-        return next;
-    }
-    
-    public void setNext(String next) {
-        this.next = next;
-    }
-    
-    public List<T> getResults() {
-        return results;
-    }
-    
-    public void setResults(List<T> results) {
-        this.results = results;
-    }
 }
 
